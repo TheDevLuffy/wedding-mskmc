0d910a287f4de32e5576b6046f74396b550596c8
thedevluffy <thedevluffy@gmail.com>
2024-02-10 22:14:00 +0900

    add header structure

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 (function () {
   function calculateDay(targetDate) {
-    const targetTime = new Date(targetDate);
-    const todayTime = new Date();
+    const targetTime = new Date(targetDate)
+    const todayTime = new Date()
 
-    const diff = targetTime - todayTime;
+    const diff = targetTime - todayTime
 
-    const diffDay = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const diffHour = Math.floor((diff / (1000 * 60 * 60)) % 24);
-    const diffMin = Math.floor((diff / (1000 * 60)) % 60);
-    const diffSec = Math.floor(diff / 1000 % 60);
+    const diffDay = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const diffHour = Math.floor((diff / (1000 * 60 * 60)) % 24)
+    const diffMin = Math.floor((diff / (1000 * 60)) % 60)
+    const diffSec = Math.floor(diff / 1000 % 60)
 
     return {
       day: diffDay,
@@ -24,6 +24,21 @@
       document.querySelector('#timer').innerText = `D-${dDay.day} ${dDay.hour}:${dDay.min}:${dDay.second}`
     }, 1000)
   }
+
+  function checkMenu() {
+    if (window.scrollY > 44) {
+      document.body.classList.add('global-nav-sticky')
+    } else {
+      document.body.classList.remove('global-nav-sticky')
+    }
+  }
+
+
+  window.addEventListener('load', () => {
+    window.addEventListener('scroll', () => {
+      checkMenu()
+    })
+  })
 
   activateTimerUI()
 })()
